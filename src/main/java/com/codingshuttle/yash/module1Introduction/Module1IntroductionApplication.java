61a9134067b029cd375d94ec4c9695020759076b
SkylineAPI <aash.pratapsingh@gmail.com>
2026-01-05 13:51:49 +0400

    beans scope

--- src/main/java/com/codingshuttle/yash/module1Introduction/Module1IntroductionApplication.java
+++ src/main/java/com/codingshuttle/yash/module1Introduction/Module1IntroductionApplication.java
@@ -11,6 +11,12 @@ public class Module1IntroductionApplication implements CommandLineRunner {
 	@Autowired
 	PayementService payementServiceObj;
 
+	@Autowired
+	PayementService payementServiceObj1;
+
+	@Autowired
+	PayementService payementServiceObj2;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Module1IntroductionApplication.class, args);
 
@@ -19,5 +25,11 @@ public class Module1IntroductionApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		payementServiceObj.pay();
+		payementServiceObj1.pay();
+		payementServiceObj2.pay();
+		System.out.println(payementServiceObj1.hashCode());
+		System.out.println(payementServiceObj2.hashCode());
 	}
+
+
 }
--- src/main/java/com/codingshuttle/yash/module1Introduction/PayementService.java
+++ src/main/java/com/codingshuttle/yash/module1Introduction/PayementService.java
@@ -1,11 +1,25 @@
 package com.codingshuttle.yash.module1Introduction;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+//@Component
+@Service
 public class PayementService {
     public void pay(){
         System.out.println("....Paying....");
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("Before Paying....");
+    }
+
+    @PreDestroy
+    public void beforeDestroy(){
+        System.out.println("After Payment....");
     }
 }
