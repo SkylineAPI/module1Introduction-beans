64db7ac84cbd477ae6f10ea448831c36b22d8037
SkylineAPI <aash.pratapsingh@gmail.com>
2026-01-05 11:53:40 +0400

    beans create and inject

--- src/main/java/com/codingshuttle/yash/module1Introduction/Module1IntroductionApplication.java
+++ src/main/java/com/codingshuttle/yash/module1Introduction/Module1IntroductionApplication.java
@@ -1,16 +1,23 @@
 package com.codingshuttle.yash.module1Introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Module1IntroductionApplication {
+public class Module1IntroductionApplication implements CommandLineRunner {
+
+	@Autowired
+	PayementService payementServiceObj;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Module1IntroductionApplication.class, args);
 
-		PayementService payementServiceobj = new PayementService();
-		payementServiceobj.pay();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		payementServiceObj.pay();
+	}
 }
--- src/main/java/com/codingshuttle/yash/module1Introduction/PayementService.java
+++ src/main/java/com/codingshuttle/yash/module1Introduction/PayementService.java
@@ -1,5 +1,9 @@
 package com.codingshuttle.yash.module1Introduction;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class PayementService {
     public void pay(){
         System.out.println("....Paying....");
